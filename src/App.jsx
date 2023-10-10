3fbc7f800baa11fc51b9718a127c220eb7bea03f
Alwin-GitRepo <alwin.melattu@gmail.com>
2023-10-10 10:23:37 +0530

    myapp

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Demo from './Demo';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App Component - Root Component</h1>
+      <Demo/>
+    </div>
+  );
+}
+
+export default App;
